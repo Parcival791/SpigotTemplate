bd5e6238ab1731d718d17feb60da7062052e26c2
Parcival791 <66206896+Parcival791@users.noreply.github.com>
2022-03-17 18:26:16 +0100

    Add files via upload

---
+++ PluginTemplate/src/main/java/net/parcival/pluginTemplate/pluginTemplate.java
@@ -0,0 +1,15 @@
+package net.parcival.pluginTemplate;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class pluginTemplate extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
